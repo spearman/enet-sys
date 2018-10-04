18b45ab08adb5bf59b945393ad2bbca8b44810c8
Shane Pearman <spearman@gitlab.com>
2018-10-03 21:01:50 -0700

    build version 1.4 on msys2

--- src/build.rs
+++ src/build.rs
@@ -4,10 +4,12 @@ fn main() {
     match &*t {
         "x86_64-pc-windows-gnu" => println!("cargo:rustc-link-lib=static=winmm"),
         "i686-pc-windows-gnu" => println!("cargo:rustc-link-lib=static=winmm"),
+        "x86_64-pc-windows-msvc" => println!("cargo:rustc-link-lib=static=winmm"),
         "x86_64-unknown-linux-gnu" => {},
         //"i686-unknown-linux-gnu" => {},
         _ => panic!("Target not supported."),
     }
     println!("cargo:rustc-link-search={}/lib/{}", dir, t);
+    println!("cargo:rustc-link-search=i:\\msys64\\usr\\lib\\w32api");
     println!("cargo:rustc-link-lib=static=enet");
 }
